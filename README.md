1fd0424cae028dceda9638f16f416bab4703f957
pm <xuemeng0318@163.com>
2024-09-04 14:33:45 +0800

    init order controller

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+%UI》P》RT￥%45^$%^>M<<cle/details/136
+%UI》P》RT￥%45^$%^>M<<cle/details/136
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'ecoTrack',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true, // 自动链接被 forFeature 注册的实体
+      synchronize: true, // 实体与表同步 调试模式下开始。不然会有强替换导致数据丢是
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
