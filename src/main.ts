612af40cb27fbee5c1e77010a0a96cfa59ccadf0
pm <xuemeng0318@163.com>
2025-02-18 17:27:22 +0800

    初始化 实现简易的CRUD

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 bootstrap();
