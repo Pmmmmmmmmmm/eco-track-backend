8a2fff92e59adcf1229e7beb1bb9606ff03b0fb6
pm <xuemeng0318@163.com>
2025-02-19 14:15:53 +0800

    关闭数据库自动同步

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    // 注册全局错误的过滤器
+    app.useGlobalFilters(new HttpExceptionFilter());
+    // 全局注册拦截器
+    app.useGlobalInterceptors(new TransformInterceptor());
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch (error) {
